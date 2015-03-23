57830476a58dab81c527f7d85f45fcba55a5d032
kraegosun@gmail.com <kraegosun@gmail.com@571c4490-01fc-a5aa-e929-d2a595e16310>
2015-03-23 15:49:31 +0000

    added blox scene

--- trunk/ProofofConcept/ProofOfConcept_Kinect/Assets/Script/Kinect/KinectModelControllers/KinectPointController.cs
+++ trunk/ProofofConcept/ProofOfConcept_Kinect/Assets/Script/Kinect/KinectModelControllers/KinectPointController.cs
@@ -47,8 +47,8 @@ public class KinectPointController : MonoBehaviour {
 		R_Arm_Chest = Right_Arm | Spine,
 		No_Feet = All & ~(Foot_Left | Foot_Right),
 		UpperBody = Shoulder_Center | Head|Shoulder_Left | Elbow_Left | Wrist_Left | Hand_Left|
-		Shoulder_Right | Elbow_Right | Wrist_Right | Hand_Right
-		
+		Shoulder_Right | Elbow_Right | Wrist_Right | Hand_Right,
+		Both_Hands =  Wrist_Right | Hand_Right | Wrist_Left | Hand_Left
 	}
 	
 	public SkeletonWrapper sw;
