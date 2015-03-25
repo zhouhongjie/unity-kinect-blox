f733897a6c86cdeac3a4923e6de0f3890b22433a
mert.ertuurul <mert.ertuurul@571c4490-01fc-a5aa-e929-d2a595e16310>
2015-03-25 08:37:47 +0000

    merged proof of concepts

---
+++ trunk/Unity_Kinect_Blox/Assets/MoveCubes.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+var forceAmount : Vector3;
+
+function Start () {
+
+}
+
+function Update () {
+GetComponent.<Rigidbody>().AddForce(forceAmount);
+transform.LookAt(Vector3.zero);
+
+
+
+
+}
+
+function OnCollisionEnter (Cubes : Collision) {
+		
+		forceAmount=-forceAmount;
+		
+		//Destroy(gameObject);
+	}
