007a47a25f3b7909ed838faa7a37652a9914550c
kraegosun@gmail.com <kraegosun@gmail.com@571c4490-01fc-a5aa-e929-d2a595e16310>
2015-04-15 09:02:51 +0000

    cleaned project contents

---
+++ trunk/Unity_Kinect_Blox/Assets/Script/BlockHandling/ShapeMe.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShapeMe : MonoBehaviour {
+	/*
+	 * Create setters for shaping the block like color, speed
+	 * 
+	 */
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
